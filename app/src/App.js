d411976d9800bd697d738ae0a976998e7765aba3
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-11-10 13:31:06 +0100

    Add very simple web page
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './form.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: {
+        dependencies: null,
+        err: null
+      }
+    };
+  }
+
+  render = () => {
+    return (
+      <div className="App container">
+        <Form onResponseHandler={this.onResponseHandler}></Form>
+      </div>
+    );
+  };
+
+  onResponseHandler = (err, response) => {
+    let s;
+    if (err !== null) {
+      s = {response: {dependencies: null, err: err}};
+    } else {
+      s = {response: {dependencies: response.getDependencies(), err: null}};
+    }
+
+    this.setState(s);
+  };
+
 }
 
 export default App;
